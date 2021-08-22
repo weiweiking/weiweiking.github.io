220e70167d16f7bac82ef1e1f8587f881561c6e4
weiweiking <1258377025@qq.com>
2021-08-22 12:29:09 +0800

    qwqw

---
+++ js/js.js
@@ -0,0 +1,14 @@
+//音乐播放js
+window.onload = function() {
+    var audio = document.getElementById('music');
+    audio.pause(); //打开页面时不播放音乐，
+}
+function play() {
+    var audio = document.getElementById('music');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0; //音乐从头播放
+    }
+}
